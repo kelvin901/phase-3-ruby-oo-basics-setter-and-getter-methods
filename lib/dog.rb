035eee77e277186ed7680788f0a5be988fcfa7f7
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-18 22:07:11 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
-class Dog
 
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
 end
+
+dog = Dog.new("sammys")
+puts dog.breed  # Output: Labrador
+
+dog.breed = "buddie"
+puts dog.breed  # Output: Poodle
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name
+      @name
+    end
+    
+    def name=(new_name)
+      @name = new_name
+    end
+    
+    def job
+      @job
+    end
+    
+    def job=(new_job)
+      @job = new_job
+    end
+  end
+  person = Person.new
 
-end
+  person.name = "Kelvin Nyoike"
+  puts person.name  # Output: John Doe
+  
+  person.job = "Software Engineer"
+  puts person.job  # Output: Software Engineer
+    
